0b4a4e4f007196af910faa4f2627deb0e57536cd
jeffwesson <jeff@jeffwesson.com>
2015-02-22 01:38:54 -0800

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    traceur: {
+      options: {
+        // traceur options here
+        experimental: true,
+        // module naming options,
+        moduleNaming: {
+          stripPrefix: "src/es6",
+          addPrefix: "com/jeffwesson/es6-fun"
+        },
+        //Turn on support for let and const
+        blockBinding: true,
+        copyRuntime: 'src/es5'
+      },
+      custom: {
+        files: [{
+          expand: true,
+          cwd: 'src/es6',
+          src: ['*.js'],
+          dest: 'src/es5'
+        }]
+      },
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      js: {
+        files: ['./scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-traceur');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit']);
+
+};
